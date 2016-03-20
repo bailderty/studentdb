0d00bd0014ebd855fecc0ef06fbed19aad35a861
brett meyer <bailderty@gmail.com>
2016-03-20 11:46:10 -0500

    Student.cpp is finished
    tookCourse() and getGPA() finished

---
+++ studentdb/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  studentdb
+//
+//  Created by Brett Meyer on 3/19/16.
+//  Copyright © 2016 BDM. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+int main (){
+    std::cout << "HELLO";
+    return 0;
+}
--- studentdb/studentdb/Student.cpp
+++ studentdb/studentdb/Student.cpp
@@ -20,8 +20,48 @@ Student::Student(int id, int numCourses)
 //TODO: Calculate the GPA from the letter grades given in CourseType
 float Student::getGPA()
 {
-    
-    return gpa;
+    if (gpa == 0) {
+        float totalGPA = 0;
+        float totalCredits = 0;
+        for (int i = 0; i< Courses.size(); i++) {
+            Coursetype c = Courses.at(i);
+            if(c.grade.compare("A")==0)
+            {
+                totalGPA = totalGPA + (4 * c.credits);
+            }
+            else if (c.grade.compare("AB")==0)
+            {
+                totalGPA = totalGPA + (3.5 * c.credits);
+            }
+            else if (c.grade.compare("B")==0)
+            {
+                totalGPA = totalGPA + (3 * c.credits);
+            }
+            else if (c.grade.compare("BC")==0)
+            {
+                totalGPA = totalGPA + (2.5 * c.credits);
+            }
+            else if (c.grade.compare("C")==0)
+            {
+                totalGPA = totalGPA + (2 * c.credits);
+            }
+            else if (c.grade.compare("D")==0)
+            {
+                totalGPA = totalGPA + (1 * c.credits);
+            }
+            else
+            {
+                totalGPA = totalGPA + (0 * c.credits);
+            }
+            totalCredits = totalCredits + c.credits;
+        }
+        gpa =  totalGPA / totalCredits;
+        return gpa;
+    }
+    else
+    {
+        return gpa;
+    }
 }
 
 int Student::getNumCourses()
@@ -42,39 +82,17 @@ void Student::addStudentCourseInfo(int cNumber, int cCredits, char * g)
     c.credits = cCredits;
     c.grade = g;
     transform(c.grade.begin(), c.grade.end(), c.grade.begin(), toupper);
-    if(c.grade.compare("A")==0)
-    {
-        
-    }
-    else if (c.grade.compare("AB")==0)
-    {
-        
-    }
-    else if (c.grade.compare("B")==0)
-    {
-        
-    }
-    else if (c.grade.compare("BC")==0)
-    {
-        
-    }
-    else if (c.grade.compare("C")==0)
-    {
-        
-    }
-    else if (c.grade.compare("D")==0)
-    {
-        
-    }
-    else
-    {
-    
-    }
-
     Courses.push_back(c);
 }
 
+
 bool Student::tookCourse(int courseNumber)
 {
+    for (int i = 0; i< Courses.size(); i++) {
+        Coursetype c = Courses.at(i);
+        if (c.courseNum == courseNumber) {
+            return true;
+        }
+    }
     return false;
 }
